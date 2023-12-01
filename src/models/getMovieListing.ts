5faad6cf4cc4cebcb468e71aed45e9f13adeb375
Jhonatan Martinez <jstevensmartinez@unicesar.edu.co>
2023-12-01 17:33:36 -0500

    Correccion de la consulta de cartelera

--- src/models/getMovieListing.ts
+++ src/models/getMovieListing.ts
@@ -11,7 +11,7 @@ export const getMovieListing = (page: number, size: number) => {
     try {
       const offset = size * page;
       const movies =
-        await prismaClient.$queryRaw`select * from Solicitud s join Pelicula P on s.id = P.solicitud_id where estado = 'APROBADA' limit ${offset},${size}`;
+        await prismaClient.$queryRaw`select * from Solicitud s join Pelicula P on s.id = P.solicitud_id where estado = 'APROBADA' and aws_url is not null  limit ${offset},${size}`;
       resolve(movies);
     } catch (error) {
       reject(error);
