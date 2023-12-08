4dae751c849d1cd7d6374be58b2411d7fb569b9b
Jhonatan Martinez <jstevensmartinez@unicesar.edu.co>
2023-12-07 22:34:59 -0500

    Correccion de la paginacion

--- src/models/getMovieListing.ts
+++ src/models/getMovieListing.ts
@@ -14,7 +14,7 @@ export const getMovieListing = (page: number, size: number) => {
       const movies =
         (await prismaClient.$queryRaw`select * from Solicitud s join Pelicula P on s.id = P.solicitud_id where estado = 'APROBADA' and aws_url is not null  limit ${offset},${size}`) as Solicitud[];
       const count =
-        (await prismaClient.$queryRaw`select count(*) as count from Solicitud s join Pelicula P on s.id = P.solicitud_id where estado = 'APROBADA' and aws_url is not null  limit ${offset},${size}`) as [
+        (await prismaClient.$queryRaw`select count(*) as count from Solicitud s join Pelicula P on s.id = P.solicitud_id where estado = 'APROBADA' and aws_url is not null`) as [
           { count: string }
         ];
 
